8ee1c5765ec04b3b3901a58babdb81197b65ee5c
Saufi <msaufi2325@gmail.com>
2024-01-28 22:13:32 +0900

    Update template file extensions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	// "github.com/msaufi2325/21_Making_web_app/pkg/config"
 	"github.com/msaufi2325/21_Making_web_app/pkg/handlers"
 )
 
@@ -11,6 +12,7 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	// var app config.AppConfig
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,12 +14,16 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	// create a template cache
 	tc, err := createTemplateCache()
 	if err != nil {
+		fmt.Println("error in createTemplateCache")
 		log.Fatal(err)
 	}
 
 	// get the requested template from cache
 	t, ok := tc[tmpl]
 	if !ok {
+		fmt.Println("error in tc[tmpl]")
+		fmt.Println(tmpl)
+		fmt.Println(tc)
 		log.Fatal(err)
 	}
 
@@ -26,12 +31,14 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	err = t.Execute(buf, nil)
 	if err != nil {
+		fmt.Println("error in t.Execute")
 		log.Println(err)
 	}
 
 	// render the template
 	_, err = buf.WriteTo(w)
 	if err != nil {
+		fmt.Println("error in buf.WriteTo")
 		log.Println(err)
 	}
 
@@ -41,29 +48,33 @@ func createTemplateCache() (map[string]*template.Template, error) {
 	// myCache := make(map[string]*template.Template)
 	myCache := map[string]*template.Template{}
 
-	// get all of the files named *.page.tmpl from the ./templates directory
-	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	// get all of the files named *.page.gohtml from the ./templates directory
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
 	if err != nil {
+		fmt.Println("error in filepath.Glob")
 		return myCache, err
 	}
 
-	// range through all files ending with *.page.tmpl
+	// range through all files ending with *.page.gohtml
 	for _, page := range pages {
 		// get the file name
 		name := filepath.Base(page)
 		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
+			fmt.Println("error in template.New")
 			return myCache, err
 		}
 
-		matches, err := filepath.Glob("./templates/*layout.tmpl")
+		matches, err := filepath.Glob("./templates/*layout.gohtml")
 		if err != nil {
+			fmt.Println("error in filepath.Glob")
 			return myCache, err
 		}
 
 		if len(matches) > 0 {
-			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
 			if err != nil {
+				fmt.Println("error in ts.ParseGlob")
 				return myCache, err
 			}
 		}
