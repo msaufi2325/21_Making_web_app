00c8bc1a64430e28d60fd07eca3c9e4fec5b8682
Saufi <msaufi2325@gmail.com>
2024-01-30 06:53:52 +0900

    Refactor main.go and update dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -29,10 +29,13 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Printf("Starting application on port %s\n", portNumber)
 
-	http.ListenAndServe(portNumber, nil)
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/msaufi2325/21_Making_web_app
 
 go 1.21.6
+
+require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
