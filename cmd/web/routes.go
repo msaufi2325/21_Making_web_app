08e3c687e5cc73a0e8e7ed4af70763461c416f62
Saufi <msaufi2325@gmail.com>
2024-01-30 07:12:32 +0900

    Update routing using go-chi/chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/msaufi2325/21_Making_web_app/pkg/config"
 	"github.com/msaufi2325/21_Making_web_app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+	mux := chi.NewRouter()
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
