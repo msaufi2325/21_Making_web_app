47861c3ff5e8cc29cafeac5a58ff3804b4413846
Saufi <msaufi2325@gmail.com>
2024-01-29 23:44:17 +0900

    Add default data to template data

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -19,6 +19,10 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
 // RenderTemplate render templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
@@ -39,6 +43,8 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
 
 	err := t.Execute(buf, td)
 	if err != nil {
