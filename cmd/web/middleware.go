75bea4978055fc36566e9a93a18eedded6dc5def
Saufi <msaufi2325@gmail.com>
2024-01-30 12:45:46 +0900

    Add WriteToConsole middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
