7cded0d31dd90ed5ac26fa0affbdd8cab4f17215
Saufi <msaufi2325@gmail.com>
2024-01-31 05:24:46 +0900

    Add session management and CSRF protection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/msaufi2325/21_Making_web_app/pkg/config"
 	"github.com/msaufi2325/21_Making_web_app/pkg/handlers"
 	"github.com/msaufi2325/21_Making_web_app/pkg/render"
@@ -12,9 +14,22 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.6
 require github.com/go-chi/chi v1.5.5
 
 require github.com/justinas/nosurf v1.1.1
+
+require github.com/alexedwards/scs/v2 v2.7.0
